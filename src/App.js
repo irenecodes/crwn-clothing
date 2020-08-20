9c78d1ffad28ecbefa993ab9d870ee4d62b7ae94
Irene <irenesis123@gmail.com>
2020-08-20 02:00:37 -0400

    [it] destructuring input attributes added. Added form-input, sign-in components, sign-in-and-sign-out page added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
-// components
+// pages
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component"
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx"
+
+// components 
 import Header from './components/header/header.component.jsx'
 
 
@@ -16,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path = "/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+import './sign-in.styles.scss'
+
+import FormInput from '../form-input/form-input.component'
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = event => {
+    //   will grab the "name" attribute defined in the input below and assign its target.value 
+      const {value, name} = event.target
+      this.setState({[name]: value})
+  }
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+ 
+            type="email"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            required
+            label="email"
+          />
+          
+          <FormInput
+      
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            required
+            label="password"
+          />
+          
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
